557c13302a71bf7b773df940fd73970da2d4bdd8
marekdubiel <pandubiel@gmail.com>
2018-02-01 13:10:58 +0100

    initiall commit

---
+++ src/main/java/eduCAS/EduCourseAttendanceSystemApplication.java
@@ -0,0 +1,12 @@
+package eduCAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduCourseAttendanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduCourseAttendanceSystemApplication.class, args);
+	}
+}
